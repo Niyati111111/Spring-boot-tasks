644eb788e06aa2f70f415fdd4c8b61b655fe86d3
Niyati111111 <51950233+Niyati111111@users.noreply.github.com>
2019-08-07 18:01:27 +0530

    Update MusixService.java

--- src/main/java/com/stackroute/musixapp/service/MusixService.java
+++ src/main/java/com/stackroute/musixapp/service/MusixService.java
@@ -10,14 +10,14 @@ public interface MusixService {
 
     public Musix saveNewMusix(Musix musix) throws TrackAlreadyExistsException;
 
-    public List<Musix> getMusix();
+    public List<Musix> getMusix() throws TrackNotFoundException;
 
     public Musix getById(int id) throws TrackNotFoundException;
 
-    public void deleteById(int id) throws TrackNotFoundException;
+    public List<Musix> deleteById(int id) throws TrackNotFoundException;
 
-    public boolean updateById(Musix musix, int id) throws TrackNotFoundException;
+    public Musix updateById(Musix musix, int id) throws TrackNotFoundException;
 
-    public List<Musix> getByName(String name);
+    public List<Musix> getByName(String name) throws TrackNotFoundException;
 
 }
